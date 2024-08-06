e24640acc7ea2a555ecbc58621fbbdec13700f14
Daniel349167 <duretae@gmail.com>
2024-08-05 20:30:22 -0500

    comentario

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CategoryController extends Controller
 {
@@ -34,5 +35,64 @@ class CategoryController extends Controller
         $category = Category::findOrFail($id);
         $category->delete();
         return response()->json(null, 204);
+    }
+
+    public function listCategories(Request $request)
+    {
+        Log::info('Request from Dialogflow', ['request' => $request->all()]);
+
+        $intentInfo = $request->input('intentInfo');
+        Log::info('Intent Info from request', ['intentInfo' => $intentInfo]);
+
+        if (!$intentInfo) {
+            return response()->json([
+                'fulfillment_response' => [
+                    'messages' => [
+                        [
+                            'text' => [
+                                'text' => ['No se pudo extraer la información del intento.']
+                            ]
+                        ]
+                    ]
+                ]
+            ], 200, ['Content-Type' => 'application/json']);
+        }
+
+        $categories = Category::all();
+        Log::info('Categories retrieved', ['categories' => $categories]);
+
+        if ($categories->isEmpty()) {
+            return response()->json([
+                'fulfillment_response' => [
+                    'messages' => [
+                        [
+                            'text' => [
+                                'text' => ['No se encontraron categorías.']
+                            ]
+                        ]
+                    ]
+                ]
+            ], 200, ['Content-Type' => 'application/json']);
+        }
+
+        $categoryNames = $categories->pluck('name');
+        Log::info('Category names', ['categoryNames' => $categoryNames]);
+
+        $responseText = 'Las categorías disponibles son: ' . $categoryNames->join(', ') . '.';
+
+        $response = [
+            'fulfillment_response' => [
+                'messages' => [
+                    [
+                        'text' => [
+                            'text' => [$responseText]
+                        ]
+                    ]
+                ]
+            ],
+            'sessionInfo' => $request->input('sessionInfo')
+        ];
+
+        return response()->json($response, 200, ['Content-Type' => 'application/json']);
     }
 }
